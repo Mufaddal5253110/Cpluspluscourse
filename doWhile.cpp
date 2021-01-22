1d7e02384aefd450435c1d830c6f615516fa2536
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-01-22 10:39:29 +0530

    do while example

---
+++ doWhile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1 , a=6;
+    do
+    {
+        int b = a*i;
+        printf("%d x %d = %d\n",a,i,b);
+        i++;
+    } while (i<11);
+    
+    return 0;
+}
