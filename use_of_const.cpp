f50adc1d11b2a1a3c4185eb0aab1e981ef03632a
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-01-23 10:15:52 +0530

    how to use const

---
+++ use_of_const.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int strlen(const char *p){
+    // Constant arguments are used when you don’t want your values to be changed or modified by the function
+    return 0;
+}
+
+int main(){
+    
+    return 0;
+}
