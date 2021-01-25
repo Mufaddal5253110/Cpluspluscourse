e3bb27ed0ea2317529d12403adcafc32b92e84d4
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-01-25 14:22:24 +0530

    sum of all subarrays

--- arrayChallenges.cpp
+++ arrayChallenges.cpp
@@ -6,21 +6,47 @@ every i from 0 to n-1 output max(a[0],a[1],...,a[i])
 
 */
 
-#include <iostream>
-#include <cmath>
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 6;
+//     int arr[] = {0,-8,1,3,-4,5};
+//     int mx = arr[0];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<mx<<' ';
+//         mx =  max(mx,arr[i+1]);
+//     }
+
+//     return 0;
+// }
+
+
+
+/*
+
+Q: sum of all subarrays
+Explanation : given an array arr[] of size n. Output
+sum of each subarray of givn array
+
+*/
+
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    int n = 6;
-    int arr[] = {0,-8,1,3,-4,5};
-    int mx = arr[0];
-
-    for (int i = 0; i < n; i++)
-    {
-        cout<<mx<<' ';
-        mx =  max(mx,arr[i+1]);
+int main(){
+    int n = 3;
+    int arr[]={1,2,2};
+    for(int i= 0;i<n;i++){
+        int total = 0;
+        for(int j=i;j<n;j++){
+            total+= arr[j];
+            cout<<total<<" ";
+        }
     }
-
     return 0;
 }
