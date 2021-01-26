313377eea56d8378e40ae67887b19bd6378056bf
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-01-26 15:03:51 +0530

    2d array initialization method-2

---
+++ 2dArray.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+
+    //allocate the array
+    int **arr = new int *[row];
+    for (int i = 0; i < row; i++)
+        arr[i] = new int[col];
+
+    cout << "Enter Elements:" << endl;
+
+    // Taking input of 2-D array
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            // ary[i][j] is then rewritten as ary[i*sizeY+j];
+            cin >> arr[i][j];
+        }
+    }
+    cout << "\n\n";
+
+    //Output of 2-D array
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            // ary[i][j] is then rewritten as ary[i*sizeY+j];
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    //deallocate the array
+    for (int i = 0; i < row; i++)
+        delete[] arr[i];
+    delete[] arr;
+
+    return 0;
+}
+
+/*OUTPUT
+
+PS E:\C++ course> cd "e:\C++ course\" ; if ($?) { g++ 2dArray.cpp -o 2dArray } ; if ($?) { .\2dArray }
+3 4
+Enter Elements:
+1 1 1 1
+2 2 2 2
+3 3 3 3
+
+
+1 1 1 1
+2 2 2 2
+3 3 3 3
+
+*/
