fcdabc5bfa8fddfd5b81c977165fd21bbea02d77
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-01-27 11:41:29 +0530

    check for palindrome ex.

---
+++ charArrayExamples.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // //-------------Check for Palindrome---------
+    // int n;
+
+    // cout << "Enter length: ";
+    // cin >> n;
+
+    // char *arr = new char[n + 1];
+
+    // cout << "Enter Word: ";
+    // cin >> arr;
+
+    // bool isPalindrome = true;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] != arr[n - 1 - i])
+    //     {
+    //         isPalindrome = !isPalindrome;
+    //         break;
+    //     }
+    // }
+    // if (isPalindrome)
+    //     cout << "Palindrome";
+    // else
+    //     cout << "Not Palindrome";
+
+    // /*OUTPUT
+
+    // PS E:\C++ course> cd "e:\C++ course\" ; if ($?) { g++ charArrayExamples.cpp -o charArrayExamples } ; if ($?) { .\charArrayExamples }
+    // Enter length: 5
+    // Enter Word: Madam
+    // Not Palindrome
+    // PS E:\C++ course> cd "e:\C++ course\" ; if ($?) { g++ charArrayExamples.cpp -o charArrayExamples } ; if ($?) { .\charArrayExamples }
+    // Enter length: 5
+    // Enter Word: madam
+    // Palindrome
+
+    // */
+
+    
+
+    return 0;
+}
