6167e9d0e84307cc529dece2c664c44c6e69a100
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2022-01-24 17:03:11 +0530

    LIS

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+Function return length of longest Increasing subsequence in an array
+*/
+
+/*
+TC : O(NlogN)
+SC : O(N)
+*/
+int longestSubsequence(int n, int a[])
+{
+    vector<int> ans;
+    ans.push_back(a[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > ans.back())
+        {
+            ans.push_back(a[i]);
+        }
+        else
+        {
+            int ind = lower_bound(ans.begin(), ans.end(), a[i]) - ans.begin();
+            ans[ind] = a[i];
+        }
+    }
+    return ans.size();
+}
